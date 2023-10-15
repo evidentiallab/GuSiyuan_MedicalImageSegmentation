5be4e8373bae359def5e4e944afaf1bd32630021
lifelongcoding <lifelongcoding@gmail.com>
2023-10-15 21:48:58 +0800

    Add argparse for command line arguments and implement model saving after each epoch

--- train.py
+++ train.py
@@ -1,6 +1,4 @@
 import os.path
-
-# from model import *
 import torch
 import monai
 from torch.utils.data import DataLoader
@@ -15,8 +13,20 @@ from monai.transforms import (
 )
 from monai.data import Dataset, list_data_collate
 from glob import glob
+import argparse
 
-data_dir = r"C:\Users\lifel\Projects\Evidential-neural-network-for-lymphoma-segmentation\Evidential_segmentation\LYMPHOMA\Data"
+parser = argparse.ArgumentParser(description="Training script for medical image segmentation")
+parser.add_argument("--epochs", type=int, default=100, help="Number of training epochs")
+parser.add_argument("--batch_size", type=int, default=1, help="Batch size for training")
+parser.add_argument("--learning_rate", type=float, default=1e-3, help="Learning rate for optimizer")
+parser.add_argument("--data_dir", type=str, default="./data", help="Directory containing the data")
+parser.add_argument("--save_dir", type=str, default="./models", help="Directory to save trained models")
+args = parser.parse_args()
+
+if not os.path.exists(args.save_dir):
+    os.makedirs(args.save_dir)
+
+data_dir = args.data_dir
 pet_dir = os.path.join(data_dir, 'SUV')
 ct_dir = os.path.join(data_dir, 'CTres')
 mask_dir = os.path.join(data_dir, 'SEG')
@@ -41,9 +51,7 @@ transforms = Compose(
 )
 
 dataset = Dataset(data=data_dicts, transform=transforms)
-
-dataloader = DataLoader(dataset, batch_size=1, num_workers=0, collate_fn=list_data_collate)
-
+dataloader = DataLoader(dataset, batch_size=args.batch_size, num_workers=0, collate_fn=list_data_collate)
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 model = UNet(
@@ -57,15 +65,15 @@ model = UNet(
 
 # params = model.parameters()
 # params = filter(lambda p: p.requires_grad, model.parameters())
-optimizer = torch.optim.Adam(model.parameters(), 1e-3)
+optimizer = torch.optim.Adam(model.parameters(), args.learning_rate)
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=10)
 loss_function = monai.losses.DiceLoss(include_background=False, softmax=True, squared_pred=True, to_onehot_y=True)
 
 epoch_loss_values = list()
 
-for epoch in range(100):
+for epoch in range(args.epochs):
     print("-" * 10)
-    print(f"epoch {epoch + 1}/{100}")
+    print(f"epoch {epoch + 1}/{args.epochs}")
     model.train()
     epoch_loss = 0
     step = 0
@@ -74,7 +82,6 @@ for epoch in range(100):
         inputs, labels = batch_data["pet_ct"].to(device), batch_data["mask"].to(device)
         optimizer.zero_grad()
         outputs = model(inputs)
-
         loss = loss_function(outputs, labels)
         loss.backward()
         optimizer.step()
@@ -83,6 +90,9 @@ for epoch in range(100):
         print(f"{step}/{epoch_len}, train_loss: {loss.item():.4f}")
     epoch_loss /= step
     epoch_loss_values.append(epoch_loss)
-
     print(f"epoch {epoch + 1} average loss: {epoch_loss:.4f}")
     scheduler.step(epoch_loss)
+
+    save_path = os.path.join(args.save_dir, f"model_epoch_{epoch+1}.pth")
+    torch.save(model.state_dict(), save_path)
+    print(f"Model saved to {save_path}")
